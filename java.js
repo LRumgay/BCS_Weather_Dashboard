30550687b63e9da4005ff8626d4b208e6cd1c22e
Liam Rumgay <Liamrumgay2@gmail.com>
2021-06-29 22:56:54 -0500

    Added JS to find city-not working

--- java.js
+++ java.js
@@ -1,15 +1,35 @@
 //Weather API//
 var APIKey = "cda2b0c406ccc575b9e9e93d4232a23d";
-var city = "Austin";
+
+
+
+
+$("button").on("click", function (event) {
+event.preventdefault();
+var city = $("#city").val();
 var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-
-
 fetch(queryURL)
-    .then(function (response) {
-        return response.json()
+.then(function (response) {
+    return response.json()
+})
+.then(function (data) {
+    console.log(data);
+
+    
+    var lat = "";
+    var lon = "";
+var queryURL2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=${APIkey}`;
+    fetch(queryURL2)
+        .then(function (response){
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
+        })
     })
-    .then(function (data) {
-        console.log(data);
-    })
-//---Weather API//
+})
+//---Current Weather API//
+
+
+
 
